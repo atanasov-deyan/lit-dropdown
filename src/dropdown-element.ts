cee7aadcd9fca4d92f1691fc9c7db7be65330477
Deyan Atanasov <deyan.atanasov@movedigital.ch>
2024-03-15 11:24:26 +0200

    wip: progress

---
+++ src/dropdown-element.ts
@@ -0,0 +1,78 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+import { when } from 'lit/directives/when.js'
+
+@customElement('dropdown-element')
+export class DropdownElement extends LitElement {
+
+  @property({ type: String })
+  label = ''
+
+  @property({ type: Boolean })
+  isOpen = false
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen
+  }
+
+  render() {
+    return html`
+      <button @click=${this.toggleDropdown}>
+        <slot></slot>
+      </button>
+      ${when(
+        this.isOpen,
+        () => html`
+          <ul>
+            <li>1</li>
+            <li>2</li>
+          </ul>
+        `
+        )}
+    `
+  }
+
+  static styles = css`
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+
+    button {
+      border-radius: 8px;
+      border: 1px solid transparent;
+      padding: 0.6em 1.2em;
+      font-size: 1em;
+      font-weight: 500;
+      font-family: inherit;
+      background-color: #2E73FA;
+      cursor: pointer;
+      transition: background-color 0.25s;
+    }
+    button:hover {
+      background-color: #005CF9;
+    }
+    button:focus,
+    button:focus-visible {
+      outline: 4px auto -webkit-focus-ring-color;
+    }
+
+    @media (prefers-color-scheme: light) {
+      a:hover {
+        color: #747bff;
+      }
+      button {
+        background-color: #f9f9f9;
+      }
+    }
+  `
+}
+
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'dropdown-element': DropdownElement
+  }
+}
